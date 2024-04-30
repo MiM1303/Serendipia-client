9d260381b95b7320bce3df4881ae4619db2b4062
Jannatul Ferdous Mirza <jfmirza1303@gmail.com>
2024-04-30 13:57:55 +0600

    added tourist section in homepage and card UI corrections

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,29 @@
+import { useLoaderData } from 'react-router-dom';
 import Banner from './Banner';
+import SpotCard from '../SpotCard/SpotCard';
 
 const Home = () => {
+    const allSpots = useLoaderData();
+    console.log(allSpots);
+    const selectedSpots = allSpots.slice(0,6);
+    console.log(selectedSpots);
     return (
         <div>
             <Banner></Banner>
-            <h2 className="text-3xl">This is Homepage</h2>
+            
+            {/* Tourist Spots Section */}
+            <div className='text-center my-8 md:my-12 lg:my-16'>
+                <h2 className='text-3xl'>Popular Tourist Spots</h2>
+                <p>Check out some of our most popular tourist spots to pick for your vacations and getaways!</p>
+            </div>
+            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 lg:gap-10'>
+                {
+                    selectedSpots.map(spot=> <SpotCard 
+                        key={spot._id}
+                        spot={spot}></SpotCard>)
+                }
+            </div>
+           
         </div>
     );
 };
--- src/pages/SpotCard/SpotCard.jsx
+++ src/pages/SpotCard/SpotCard.jsx
@@ -3,25 +3,37 @@ import { FaUsers } from "react-icons/fa6";
 import { MdOutlineTimer } from "react-icons/md";
 import { FaDollarSign } from "react-icons/fa";
 import { FaFly } from "react-icons/fa";
+import { IoLocationSharp } from "react-icons/io5";
+import { BsArrowRight } from "react-icons/bs";
+import { TbCurrencyDollar } from "react-icons/tb";
 
 const SpotCard = ({spot}) => {
-    const {_id, photo, name, cost, season, duration, visitors, description} = spot
+    const {_id, photo, name, country, cost, location, season, duration, visitors, description} = spot
 
     return (
         <div>
-            <div className="card bg-gradient-to-b from-[#caf0e0] to-[#e2edfa] shadow-xl mb-3">
-                <figure><img src={photo} className="md:w-11/12 lg:w-11/12 rounded-xl mt-5   lg:h-[335px]"  /></figure>
+            {/* <div className="card relative bg-gradient-to-b from-[#e0f2eb] to-[#f5f4e0] shadow-xl mb-3"> */}
+            <div className="card rounded-sm relative border border-base-300 shadow-sm mb-3">
+                <figure><img src={photo} className="h-[280px] w-full"  /></figure>
+                <div className="badge absolute right-6 top-6 p-4 bg-base-300 flex gap-2 textarea-lg"><IoLocationSharp className="text-red-600"/> <span>{name}</span></div>
+
                 <div className="flex flex-col gap-4 card-body">
-                    <h2 className="card-title md:text-xl lg:text-2xl">{}</h2>
-                    <p className="md:text-base lg:text-lg flex items-center gap-2"><FaFly className="text-blue-800 text-2xl" />{name}</p>
-                    <p className="md:text-base lg:text-lg flex items-center gap-2"><FaDollarSign className="text-red-700 text-2xl" />{cost}</p>
-                    <p className="md:text-base lg:text-lg flex items-center gap-2"><FaUsers className="text-green-700 text-2xl" />{visitors}</p>
-                    <p className="md:text-base lg:text-lg flex items-center gap-2"><MdOutlineTimer className="text-blue-800 text-2xl" />{duration}</p>
-                    <p className="md:text-base lg:text-lg flex items-center gap-2"><MdOutlineTimer className="text-blue-800 text-2xl" />{season}</p>
-                    <p className="md:text-base lg:text-lg flex items-center gap-2"><MdOutlineTimer className="text-blue-800 text-2xl" />{description}</p>
+                    <div className="flex justify-between ">
+                        <p className="w-2/3 md:text-base lg:text-xl flex items-center gap-2 font-semibold">{location}, {country}</p>
+                        <p className="justify-end md:text-base lg:text-lg flex items-center gap-0 text-blue-500"><TbCurrencyDollar className="text-2xl font-bold" /><span className="font-semibold">{cost}</span></p>
+                    </div>
+                    <p className="md:text-base lg:text-lg flex items-center gap-2 lg:mb-4">{description}</p>
+
+                    <div className="flex">
+                        <p className="md:text-base lg:text-lg flex items-center gap-2"><span className="font-semibold">Duration:</span> {duration} days</p>
+                        <p className="justify-end md:text-base lg:text-lg flex items-center gap-2"><span className="font-semibold">Visit during:</span> {season}</p>
+                    </div>
+                    
+                    <p className="md:text-base lg:text-lg flex items-center gap-2 "><span className="font-semibold">Visitors per Year:</span> {visitors}</p>
+                    <hr className="mb-2 mt-3"/>
                     <div className="card-actions justify-end">
                         <Link to={`/all-spots/${_id}`}>
-                            <button className="btn bg-[#50a9af] text-white text-lg hover:bg-[#2e4057]">View Spot Details</button>
+                            <button className="bg-transparent text-[#A98467] border-none text-lg flex items-center gap-2"><span>View Spot Details</span> <BsArrowRight className="text-3xl font-thin"/></button>
                         </Link>
                     </div>
                     
